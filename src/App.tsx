3c3efbde1446450c8dbb5a48eeb79deb78796939
Giscard <ngabogiscard@hotmail.com>
2026-02-09 15:02:51 +0200

    update

--- src/App.tsx
+++ src/App.tsx
@@ -207,12 +207,12 @@ export default function App() {
 
           {/* Couple Image */}
           <div className="w-full rounded-[2rem] overflow-hidden shadow-2xl shadow-black/10 animate-scaleIn" style={{ animationDelay: '0.6s' }}>
-            
+        
             <img
-              src="/src/asset/Photo1.jpeg"
-              alt="Couple"
-              className="w-full h-auto grayscale hover:grayscale-0 transition-all duration-700"
-            />
+           src='/src/asset/Photo1.jpeg'
+          alt="Couple"
+             className="w-full h-auto grayscale hover:grayscale-0 transition-all duration-700"
+               />
           </div>
         </div>
       </section>
